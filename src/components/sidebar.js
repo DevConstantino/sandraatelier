dbfcfa5847b39b3a17adeb6b289d7aa8851dea88
Matheus Constantino <dev.constantino@gmail.com>
2022-02-23 11:37:37 -0300

    sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -2,30 +2,57 @@ import { SiGmail } from "react-icons/si";
 import { FiInstagram } from "react-icons/fi";
 import { FaFacebookF } from "react-icons/fa";
 import { FaShoppingCart } from "react-icons/fa";
-import { Link } from "react-router-dom";
 
 function Sidebar() {
 	return (
 		<ul class="nav justify-content-center flex-column">
 			<li class="nav-item py-2">
-				<Link class="nav-link active" aria-current="page" to="">
-					<SiGmail />
-				</Link>
+				<button className="btn-outline-primary btnSidebar shadow-lg animation01">
+					<a
+						target="_blank"
+						class="nav-link active m-0 p-0 sideIcons"
+						aria-current="page"
+						href="#"
+					>
+						<SiGmail />
+					</a>
+				</button>
 			</li>
 			<li class="nav-item py-2">
-				<Link class="nav-link" to="">
-					<FiInstagram />
-				</Link>
+				<button className="btn-outline-primary btnSidebar shadow-lg animation02">
+					<a
+						target="_blank"
+						class="nav-link active m-0 p-0 sideIcons"
+						aria-current="page"
+						href="#"
+					>
+						<FiInstagram />
+					</a>
+				</button>
 			</li>
 			<li class="nav-item py-2">
-				<Link class="nav-link" to="">
-					<FaFacebookF />
-				</Link>
+				<button className="btn-primary btnSidebar shadow-lg animation01">
+					<a
+						target="_blank"
+						class="nav-link active m-0 p-0 sideIcons"
+						aria-current="page"
+						href="#"
+					>
+						<FaFacebookF />
+					</a>
+				</button>
 			</li>
 			<li class="nav-item py-2">
-				<Link class="nav-link" to="">
-					<FaShoppingCart />
-				</Link>
+				<button className="btn-outline-primary btnSidebar shadow-lg animation02">
+					<a
+						target="_blank"
+						class="nav-link active m-0 p-0 sideIcons"
+						aria-current="page"
+						href="#"
+					>
+						<FaShoppingCart />
+					</a>
+				</button>
 			</li>
 		</ul>
 	);
